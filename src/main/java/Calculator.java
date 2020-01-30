291f4eb2e7e0ade89e3a30be9318619c274e26ca
Liam - g00364974 <g00364974@gmit.ie>
2020-01-30 11:54:08 +0000

    This is my second commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,5 +5,19 @@ public class Calculator {
         sum = fistrNum+secondNum;
         return sum;
     }
-
+    public int Multiply(int firtsNum, int secondNum){
+        int result = 0;
+        result = firtsNum * secondNum;
+        return result;
+    }
+    public int Divide(int firtsNum, int secondNum){
+        int result = 0;
+        result = firtsNum / secondNum;
+        return result;
+    }
+    public int substract(int fistrNum, int secondNum){
+        int sum = 0;
+        sum = fistrNum-secondNum;
+        return sum;
+    }
 }
